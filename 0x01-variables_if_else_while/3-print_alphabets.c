503e993bf591a6a866286e5581d3f88d5832de55
Faith <faithlekie17@gmail.com>
2022-06-03 18:27:32 -0700

    alphabet3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+TASK 3
+
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: random number in n +ve/-ve
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int firstSLetter, firstCletter, n;
+for (firstSLetter = 97, n = 1; n < 27; firstSLetter++, n++)
+{
+putchar(firstSLetter);
+}
+for (firstCletter = 65, n = 1; n < 27; firstCletter++, n++)
+{
+putchar(firstCletter);
+}
+putchar(10);
+return (0);
+}
