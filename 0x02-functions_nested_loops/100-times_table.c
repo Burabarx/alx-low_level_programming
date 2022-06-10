989750687f6eff203a61057ee0058724f3750c9a
Burabarx <faithlekie17@gmail.com>
2022-06-09 21:13:08 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,53 +2,50 @@
 #include "main.h"
 
 /**
- * print_times_table - prints time table
- * @n: The input integer
+ * print_times_table - Print the n times table, starting from 0
+ * @n: The times table to print
  *
- *
- * Return: The value of n
+ * Return: Nothing
  */
-
 void print_times_table(int n)
 {
-int i, j, res;
+	int x, y, mult, one, ten, hund;
 
-if (!(n > 15 || n < 0))
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-res = (i * j);
-if (j != 0)
-{
-putchar(',');
-putchar(' ');
-}
-if (res < 10 && j != 0)
-{
-putchar(' ');
-putchar(' ');
-putchar((res % 10) + '0');
-}
-else if (res >= 10 && res < 100)
-{
-putchar(' ');
-putchar((res / 10) + '0');
-putchar((res % 10) + '0');
-}
-else if (res >= 100 && j != 0)
-{
-putchar((res / 100) + '0');
-putchar((res / 10) % 10 + '0');
-putchar((res % 10) + '0');
-}
-else
-{
-putchar((res % 10) + '0');
-}
-}
-printf("\n");
-}
-}
+	x = 0;
+	if (n >= 0 && n <= 15)
+	{
+		while (x <= n)
+		{
+			y = 0;
+			while (y <= n)
+			{
+				mult = x * y;
+				one = mult % 10;
+				ten = mult % 100 / 10;
+				hund = mult / 100;
+				if (hund == 0 && y != 0)
+				{
+					_putchar(' ');
+					if (ten == 0)
+						_putchar(' ');
+					else
+						_putchar(ten + '0');
+				}
+				else if (hund != 0)
+				{
+					_putchar(hund + '0');
+					_putchar(ten + '0');
+				}
+				_putchar(one + '0');
+				if (y != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				y++;
+			}
+			_putchar('\n');
+			x++;
+		}
+	}
 }
