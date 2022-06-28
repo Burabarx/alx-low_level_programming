1a3fe28518fde88350a05223f453d7fe0e28cd9b
Burabarx <faithlekie17@gmail.com>
2022-06-28 07:30:00 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,57 +1,39 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 /**
- * strtow - splits a string to words
- * @str: string to split
- * Return: a point to an array of strings or NULL
+ * *_realloc - reallocate memory size function
+ * @ptr: pointer to address of old memory location
+ * @old_size: unsigned int type of old memory size
+ * @new_size: unsigned int type for new memory size
+ * Return:  return pointer to array
  */
-char **strtow(char *str)
-{
-	char **arr_words = NULL;
-	int i, j = 0, wlen, slen, words = 0, sig = 0, pre_sig = 0;
 
-	if (str == NULL)
-		return (NULL);
-	slen = strlen(str);
-	for (i = 0; i < slen; i++)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *s;
+
+	if (new_size > old_size)
 	{
-		sig = (str[i] == 32 || str[i] == '\t') ? 0 : 1;
-		words = (pre_sig == 0 && sig == 1) ? words + 1 : words;
-		pre_sig = sig;
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
 	}
-	if (words == 0)
-		return (NULL);
-	arr_words = malloc(words * sizeof(char *));
-	if (arr_words == NULL)
+	if (new_size == old_size)
 	{
-		free(arr_words);
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		s = malloc(new_size);
+		free(ptr);
+		return (s);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
 	}
-	words = 0;
-	for (i = 0; i < slen; i++)
-	{
-		sig = (str[i] == 32 || str[i] == 9) ? 0 : 1;
-		if (sig)
-		{
-			for (j = 0; str[i + j] != 32 && str[i + j] != 9; j++)
-				;
-			wlen = j;
-			arr_words[words] = malloc(wlen * sizeof(char));
-			if (arr_words[words] == NULL)
-			{
-				for (; words >= 0; words--)
-					free(arr_words[words]);
-				free(arr_words);
-				return (NULL);
-			}
-			for (j = 0; j < wlen; j++)
-			{
-				arr_words[words][j] = str[i + j];
-			}
-			words++;
-			i += wlen - 1;
-		}
-	}
-	return (arr_words);
+	return (ptr);
 }
