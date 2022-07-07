6da60c897a3a48b6a414f8dbf7277cc45fdf48d7
Burabarx <faithlekie17@gmail.com>
2022-07-06 17:33:06 -0700

    function given as a parameter on each element of an array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Print a name from a function pointer
- * @name: char string
- * @f: function pointer that takes a string argument
+ * print_name - a function that prints a name.
+ * @name: pointer to varable.
+ * @f: function pointer.
+ * Return: Nothing.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+
+	if (f != NULL)
+
+		f(name);
+
 }
