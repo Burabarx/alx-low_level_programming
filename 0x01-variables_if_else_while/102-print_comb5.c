f1130c11a437a04ddfd598bd68ab81f3b03ece2c
Faith <faithlekie17@gmail.com>
2023-07-14 18:00:08 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,9 +19,9 @@ int main(void)
 	for (p = 0; p <= 98; p++)
 	{
 
-	for (q = p + 1; q <+ 99; q++)
+	for (q = p + 1; q <= 99; q++)
 	{
-	putchar((p / 10) +'0');
+	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
 	putchar(' ');
 	putchar((q / 10) + '0');
